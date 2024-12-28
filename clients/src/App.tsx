0e06ff226e9ee8ea741d539d117f41412175ada2
UncleCM <earth.thuwanon@gmail.com>
2024-12-29 04:48:26 +0700

    hot fix

--- clients/src/App.tsx
+++ clients/src/App.tsx
@@ -1,13 +1,13 @@
-import React from 'react';
-import { Box } from "@chakra-ui/react";
-import CodeEditor from "./components/CodeEditor";
+  import React from 'react';
+  import { Box } from "@chakra-ui/react";
+  import CodeEditor from "./components/CodeEditor";
 
-const App: React.FC = () => {
-  return (
-    <Box minH="100vh" bg="#0f0a19" color="gray.500" px={6} py={8}>
-      <CodeEditor />
-    </Box>
-  );
-};
+  const App: React.FC = () => {
+    return (
+      <Box minH="100vh" bg="#0f0a19" color="gray.500" px={6} py={8}>
+        <CodeEditor />
+      </Box>
+    );
+  };
 
-export default App;
+  export default App;
